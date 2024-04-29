d1c4ef2d10e52ab726588e2539b870e2d9683466
trailxyz <148996830+trailxyz@users.noreply.github.com>
2024-04-29 14:06:54 +0200

    upload-projekta

---
+++ App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace VjezbaGithub
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
